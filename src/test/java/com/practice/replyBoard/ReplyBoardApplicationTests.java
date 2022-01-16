1e8bae6a3a52babcffbab2eb514e5d6bc11611cd
oij511 <je25th@kakao.com>
2022-01-16 23:08:57 +0900

    Initial commit

---
+++ src/test/java/com/practice/replyBoard/ReplyBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.replyBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplyBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
